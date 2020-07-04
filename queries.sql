aea81c37e968972fa1ea0cf4d258d64db43f55a4
Hayley Smith <hayley.j.smith@gmail.com>
2020-07-03 22:52:10 -0500

    secondcommit

--- queries.sql
+++ queries.sql
@@ -1,7 +1,12 @@
 ## Part 1: Test it with SQL
+idjob - int PK
+id - int
+employer - varChar(255)
+name - varChar(255)
+skills - varChar(255)
 
 ## Part 2: Test it with SQL
-
+SELECT name FROM employer WHERE location="St. Louis City";
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull(message = "Please enter a location")
+    @ManyToOne
+    private String location;
 
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
+    
 }
