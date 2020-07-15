31f896b53bf50c4e9d10384249cc12ff06f4d459
Hayley Smith <hayley.j.smith@gmail.com>
2020-07-14 20:54:07 -0500

    finalcommit

--- queries.sql
+++ queries.sql
@@ -12,3 +12,11 @@ SELECT name FROM employer WHERE location="St. Louis City";
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
+
+SELECT skill.name, skill.description
+FROM skill
+INNER JOIN job_skills
+ON skill.id = job_skills.skills_id
+INNER JOIN job
+ON job_skills.jobs_id = job.id
+ORDER BY skill.name
